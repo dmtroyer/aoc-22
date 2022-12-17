3a714b175ac24769f1cd94a0669c3cb1db091977
David Troyer <dmtroyer@gmail.com>
2022-12-17 17:45:04 -0600

    day 6 part 1

---
+++ day-6/d6-p1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "log"
+  "os"
+  "strings"
+)
+
+func containsDups(s string) bool {
+  for _, c := range s {
+    if strings.Count(s, string(c)) > 1 {
+      return true
+    }
+  }
+  return false
+}
+
+func parseLines() []string {
+  scanner := bufio.NewScanner(os.Stdin)
+  var lines []string
+
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+
+  err := scanner.Err()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return lines
+}
+
+func main() {
+  lines := parseLines()
+
+  for _, line := range lines {
+    for i := 4; i <= len(line); i++ {
+      if !containsDups(line[i-4:i]) {
+        fmt.Println(i)
+        break
+      }
+    }
+  }
+
+}
