c867cc9c1c5db23ff38dd1d84961775f4f68faf5
David Troyer <dmtroyer@gmail.com>
2022-12-17 17:06:31 -0600

    bit o cleanup on aisle 5

--- day-5/d5-p2.go
+++ day-5/d5-p2.go
@@ -9,9 +9,9 @@ import (
 )
 
 type CraneInstruction struct {
-  number int
-  source int
-  destination int
+  num_crates int
+  src_stack int
+  dest_stack int
 }
 
 func parseInstructions(lines []string) ([]CraneInstruction) {
@@ -19,10 +19,10 @@ func parseInstructions(lines []string) ([]CraneInstruction) {
 
   for _, line := range lines {
     var inst CraneInstruction
-    _, err := fmt.Sscanf(line, "move %d from %d to %d", &inst.number, &inst.source, &inst.destination)
+    _, err := fmt.Sscanf(line, "move %d from %d to %d", &inst.num_crates, &inst.src_stack, &inst.dest_stack)
     if err == nil {
-      inst.source--
-      inst.destination--
+      inst.src_stack--
+      inst.dest_stack--
       instructions = append(instructions, inst)
     }
   }
@@ -46,18 +46,19 @@ func parseLines() []string {
   return lines
 }
 
-func parseStacks(lines []string) ([][]string) {
-  var stacks [][]string
+func parseStacks(lines []string) ([][]byte) {
+  var stacks [][]byte
 
   for i := 0; strings.Contains(lines[i], "["); i++ {
     line := lines[i]
     for j := 1; j < len(line); j += 4 {
       k := (j - 1) / 4
       if len(stacks) == k {
-        stacks = append(stacks, []string{})
+        stacks = append(stacks, []byte{})
       }
       if line[j] != ' ' {
-        stacks[k] = append([]string{string(line[j])}, stacks[k]...)
+        line_as_slice := []byte{line[j]}
+        stacks[k] = append(line_as_slice, stacks[k]...)
       }
     }
   }
@@ -70,14 +71,19 @@ func main() {
   stacks := parseStacks(lines)
   instructions := parseInstructions(lines)
 
-  for _, inst := range instructions {
-    src := &stacks[inst.source]
-    dest := &stacks[inst.destination]
+  for _, i := range instructions {
+    src := &stacks[i.src_stack]
+    dest := &stacks[i.dest_stack]
 
-    moved := (*src)[len(*src) - inst.number:]
-    *src = (*src)[:len(*src) - inst.number]
+    bottom_crate := len(*src) - i.num_crates
+    moved := (*src)[bottom_crate:]
+    *src = (*src)[:bottom_crate]
     *dest = append(*dest, moved...)
   }
 
-  fmt.Println(stacks)
+  var answer string
+  for _, stack := range stacks {
+    answer += string(stack[len(stack)-1])
+  }
+  fmt.Println(answer)
 }
