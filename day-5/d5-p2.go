129f84e3c119d4b1b65999633bf7e1d825639d63
David Troyer <dmtroyer@gmail.com>
2022-12-17 15:38:39 -0600

    spaces

--- day-5/d5-p2.go
+++ day-5/d5-p2.go
@@ -5,29 +5,29 @@ import (
   "fmt"
   "log"
   "os"
-	"strings"
+  "strings"
 )
 
 type CraneInstruction struct {
-	number int
-	source int
-	destination int
+  number int
+  source int
+  destination int
 }
 
 func parseInstructions(lines []string) ([]CraneInstruction) {
-	instructions := []CraneInstruction{}
+  instructions := []CraneInstruction{}
 
-	for _, line := range lines {
-		var inst CraneInstruction
-		_, err := fmt.Sscanf(line, "move %d from %d to %d", &inst.number, &inst.source, &inst.destination)
-		if err == nil {
-			inst.source--
-			inst.destination--
-			instructions = append(instructions, inst)
-		}
-	}
+  for _, line := range lines {
+    var inst CraneInstruction
+    _, err := fmt.Sscanf(line, "move %d from %d to %d", &inst.number, &inst.source, &inst.destination)
+    if err == nil {
+      inst.source--
+      inst.destination--
+      instructions = append(instructions, inst)
+    }
+  }
 
-	return instructions
+  return instructions
 }
 
 func parseLines() []string {
@@ -43,40 +43,38 @@ func parseLines() []string {
     log.Fatal(err)
   }
 
-	return lines
+  return lines
 }
 
 func parseStacks(lines []string) ([][]string) {
-	var stacks [][]string
+  var stacks [][]string
 
-	for i := 0; strings.Contains(lines[i], "["); i++ {
-		line := lines[i]
-		for j := 1; j < len(line); j += 4 {
-			k := (j - 1) / 4
-			if len(stacks) == k {
-				stacks = append(stacks, []string{})
-			}
-			if line[j] != ' ' {
-				stacks[k] = append([]string{string(line[j])}, stacks[k]...)
-			}
-		}
-	}
+  for i := 0; strings.Contains(lines[i], "["); i++ {
+    line := lines[i]
+    for j := 1; j < len(line); j += 4 {
+      k := (j - 1) / 4
+      if len(stacks) == k {
+        stacks = append(stacks, []string{})
+      }
+      if line[j] != ' ' {
+        stacks[k] = append([]string{string(line[j])}, stacks[k]...)
+      }
+    }
+  }
 
-	return stacks
+  return stacks
 }
 
 func main() {
-	lines := parseLines()
-	stacks := parseStacks(lines)
-	instructions := parseInstructions(lines)
+  lines := parseLines()
+  stacks := parseStacks(lines)
+  instructions := parseInstructions(lines)
 
-	for _, inst := range instructions {
-		for i := 0; i < inst.number; i++ {
-			var x string
-			x, stacks[inst.source] = stacks[inst.source][len(stacks[inst.source]) - 1], stacks[inst.source][:len(stacks[inst.source]) - 1]
-			stacks[inst.destination] = append(stacks[inst.destination], x)
-		}
-	}
+  for _, inst := range instructions {
+    var x string
+    x, stacks[inst.source] = stacks[inst.source][len(stacks[inst.source]) - 1], stacks[inst.source][:len(stacks[inst.source]) - 1]
+    stacks[inst.destination] = append(stacks[inst.destination], x)
+  }
 
   fmt.Println(stacks)
 }
