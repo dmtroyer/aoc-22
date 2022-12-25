f17fd77905e5ff607efdfe619f5d4699303e2dbd
David Troyer <dmtroyer@gmail.com>
2022-12-25 16:33:44 -0600

    d12-p1, BFS FTW.

--- day-12/d12-p1.go
+++ day-12/d12-p1.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"sort"
 	"strings"
 )
 
 const Alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 type Point struct {
-	x int
-	y int
-	prev *Point
+	x         int
+	y         int
+	parent    *Point
+	elevation int
+	explored  bool
 }
 
 func parseLines() []string {
@@ -33,94 +34,100 @@ func parseLines() []string {
 	return lines
 }
 
-var matrix = [][]int{}
-var start, antenna = Point{}, Point{}
-var pathways = []int{}
+var matrix = [][]*Point{}
+var antenna, start *Point
 var m_height, m_width = 0, 0
-
-func havePreviouslyVisited(node Point, neighbor Point) bool {
-	for node.prev != nil {
-		if pointsEqual(*node.prev, neighbor) {
-			return true
-		}
-		node = *node.prev
-	}
-	return false
-}
 
 func parseMatrix(lines []string) {
 	for i, line := range lines {
-		row := make([]int, len(line))
+		row := make([]*Point, len(line))
+
 		for j, c := range line {
 			if c == 'S' {
-				start = Point{x: j, y: i}
-				row[j] = 0
+				start = &Point{x: j, y: i, elevation: 0, explored: false}
+				row[j] = start
 			} else if c == 'E' {
-				antenna = Point{x: j, y: i}
-				row[j] = 25
+				antenna = &Point{x: j, y: i, elevation: 25, explored: false}
+				row[j] = antenna
 			} else {
-				row[j] = strings.Index(Alphabet, string(c))
+				e := strings.Index(Alphabet, string(c))
+				row[j] = &Point{x: j, y: i, elevation: e, explored: false}
 			}
 		}
 		matrix = append(matrix, row)
 	}
+
 	m_height = len(lines)
-	m_width  = len(matrix[0])
+	m_width = len(matrix[0])
 }
 
-func onGrid(p Point) bool {
-	return p.x >= 0 && p.y >= 0 && p.x < m_width && p.y < m_height
+func onGrid(x int, y int) bool {
+	return x >= 0 && y >= 0 && x < m_width && y < m_height
 }
 
-func pathLength(p Point) int {
+func pathLength(p *Point) int {
 	i := 0
-	for p.prev != nil {
+	for p.parent != nil {
 		i++
-		p = *p.prev
+		p = p.parent
 	}
 	return i
 }
 
-func pointElevation(p Point) int {
-	return matrix[p.y][p.x]
+func printMatrix() {
+	for _, row := range matrix {
+		for _, p := range row {
+			if p.elevation < 10 {
+				fmt.Print(" ")
+			}
+			fmt.Print(p.elevation, " ")
+		}
+		fmt.Print("\n")
+	}
 }
 
-func pointsEqual(p1 Point, p2 Point) bool {
-	return p1.x == p2.x && p1.y == p2.y
+func neighborIsReachable(current *Point, destination *Point) bool {
+	return destination.elevation-current.elevation <= 1
 }
 
-func withinOneElevation(current Point, destination Point) bool {
-	diff := pointElevation(destination) - pointElevation(current)
-	return diff <= 1
-}
-
-func stepOptions(p Point) []Point {
-	options := []Point{}
-	neighbors := []Point{
-		Point{x: p.x, y: p.y+1},
-		Point{x: p.x, y: p.y-1},
-		Point{x: p.x+1, y: p.y},
-		Point{x: p.x-1, y: p.y},
+func reachableNeighbors(p *Point) []*Point {
+	reachable_neighbors := []*Point{}
+	relative_neighbors := [][]int{
+		{0, 1}, {0, -1}, {1, 0}, {-1, 0},
 	}
 
-	for _, n := range neighbors {
-		if onGrid(n) && withinOneElevation(p, n) && !havePreviouslyVisited(p, n) {
-			options = append(options, n)
+	for _, n := range relative_neighbors {
+		if onGrid(p.x+n[0], p.y+n[1]) {
+			neighbor := matrix[p.y+n[1]][p.x+n[0]]
+			if neighborIsReachable(p, neighbor) {
+				reachable_neighbors = append(reachable_neighbors, neighbor)
+			}
 		}
 	}
 
-	return options
+	return reachable_neighbors
 }
 
-func findPath(p Point) {
-	if pointsEqual(p, antenna) {
-		pathways = append(pathways, pathLength(p))
-	} else {
-		for _, n := range stepOptions(p) {
-			n.prev = &p
-			findPath(n)
+func search() *Point {
+	queue := []*Point{}
+	start.explored = true
+	queue = append(queue, start)
+
+	for len(queue) > 0 {
+		p := queue[0]
+		queue = queue[1:]
+		if p == antenna {
+			return p
+		}
+		for _, n := range reachableNeighbors(p) {
+			if !n.explored {
+				n.explored = true
+				n.parent = p
+				queue = append(queue, n)
+			}
 		}
 	}
+	return nil
 }
 
 func main() {
@@ -131,8 +138,12 @@ func main() {
 	parseMatrix(lines)
 
 	fmt.Println("Starting to explore...")
-	findPath(start)
+	end := search()
 
-	sort.Ints(pathways)
-	fmt.Println(pathways[0])
+	if end == nil {
+		log.Fatal("Could not find a path.")
+	}
+
+	fmt.Print("End: ", end.x, ",", end.y, "\n")
+	fmt.Printf("Path length: %d\n", pathLength(end))
 }
